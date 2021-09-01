8cc69fd420a8f814de6b98c698fbbae7da24ada7
jorgeDPH <38698350+jorgeDPH@users.noreply.github.com>
2021-08-31 21:53:49 -0500

    Routes
    commit to Routes

--- Lab-Web/proyecto1/src/App.js
+++ Lab-Web/proyecto1/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import "./App.css";
 import "./index.css";
 import React, { useState, useContext,useEffect } from "react";
@@ -46,3 +47,44 @@ function App() {
 }
 
 export default App;
+=======
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import { Home,About, Dashboard } from "./components/NewInput";
+
+export default function BasicExample() {
+
+  return (
+
+    <Router>
+
+      <Link style={{ padding: "30px" }} to="/">Home</Link>
+
+      <Link style={{ padding: "30px" }} to="/about">About</Link>
+
+      <Link style={{ padding: "30px" }} to="/dashboard">Dashboard</Link>
+
+      <hr />
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+      </Switch>
+
+    </Router>
+  );
+}
+>>>>>>> Stashed changes
---
+++ Lab-Web/proyecto1/src/components/NewInput.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Home() {
+    return (
+      <div>
+        <h2>Home</h2>
+      </div>
+    );
+  }
+  
+  function About() {
+    return (
+      <div>
+        <h2>About</h2>
+      </div>
+    );
+  }
+  
+  function Dashboard() {
+    return (
+      <div>
+        <h2>Dashboard</h2>
+      </div>
+    );
+  }
+
+export {Home,About,Dashboard} ;
