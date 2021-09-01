7243c4b79876b5651f95707f38a1832634991685
jorgeDPH <38698350+jorgeDPH@users.noreply.github.com>
2021-08-31 22:00:04 -0500

    Routes
    commit to Routes

--- Lab-Web/proyecto1/src/App.js
+++ Lab-Web/proyecto1/src/App.js
@@ -1,53 +1,3 @@
-<<<<<<< Updated upstream
-import "./App.css";
-import "./index.css";
-import React, { useState, useContext,useEffect } from "react";
-function App() {
-  
-  const [users, setUsers] = useState([{ id: 0, name: "Jorge" }]);
-
-  const onClick = () => {
-    setUsers ([...users,{id: users.length, name: "Roa"}])
-  };  
-
-  var dataComponent={
-    type:"button",
-    value:"update",
-    hidden:false
-  }
-
-  const [date, setDate] = useState(new Date());
-
-  useEffect(() =>{
-
-    var timerID = setInterval(()=> tick(),1000)
-
-    return function cleanup(params){
-      clearInterval(timerID)
-    }
-  });
-
-  function tick(){
-    setDate(new Date());
-  }
-
-  return (
-    <div>
-      <input onClick={onClick} {...dataComponent} />
-      <div>
-        <h1>{date.toLocaleTimeString()}</h1>
-        {users.map((e) => (
-          <div>
-            {e.id} - {e.name}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
-
-export default App;
-=======
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -85,6 +35,7 @@ export default function BasicExample() {
       </Switch>
 
     </Router>
+
+    
   );
 }
->>>>>>> Stashed changes
